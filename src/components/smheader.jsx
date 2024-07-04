78d6cd89789ca11488a5668c993913f4301ea775
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-07-04 23:26:23 +0100

    new

--- src/components/smheader.jsx
+++ src/components/smheader.jsx
@@ -1,36 +1,45 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary sm-screen-head">
+import React from "react"
+import img from '../assets/img.jpg'
+import { IoIosLogOut } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { Mylist } from "./list";
+export const HeaderSm = ({displaylist,selectedListIndex, handleListLinkClick}) => {
+    return(
+<div className="navbar navbar-expand-lg bg-body-tertiary d-lg-none d-md-block sm-screen-head fixed-top">
+<nav class="navbar  d-lg-none d-md-block sm-screen-head">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider" /></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+  <RxHamburgerMenu data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" />
+    
+  
+    <div className="d-flex">
+  <div className="img-contaier-sm">
+   <img src={img} className="circle-img" />
   </div>
+  <div className="log-out">
+  <IoIosLogOut className="log-out-icon" />
+
+  </div>
+
+    </div>
+    </div>
+  
 </nav>
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Your List</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+  <div className="content">
+                <Mylist
+                  displaylist={displaylist}
+                  selectedListIndex={selectedListIndex}
+                  handleListLinkClick={handleListLinkClick}
+                />
+              </div>
+  </div>
+</div>
+</div>
+
+    )
+}
